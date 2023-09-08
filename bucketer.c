9a9b73539ab76a0617d9f2e205a656e50e6761f7
themeti30 <121438606+themeti30@users.noreply.github.com>
2023-09-08 19:55:27 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -9,6 +9,20 @@ struct CountsBySoH {
 
 struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
   struct CountsBySoH counts = {0, 0, 0};
+   for (int i = 0; i < nBatteries; i++) {
+    int ratedCapacity = 120; // Rated capacity of a new battery
+    int presentCapacity = presentCapacities[i];
+    double sohPercentage = 100.0 * presentCapacity / ratedCapacity;
+
+    if (sohPercentage > 80.0 && sohPercentage <= 100.0) {
+      counts.healthy++;
+    } else if (sohPercentage > 65.0 && sohPercentage <= 80.0) {
+      counts.exchange++;
+    } else {
+      counts.failed++;
+    }
+  }
+
   return counts;
 }
 
@@ -22,8 +36,20 @@ void testBucketingByHealth() {
   assert(counts.failed == 1);
   printf("Done counting :)\n");
 }
+void testAdditionalCase() {
+  const int presentCapacities[] = {110, 80, 75, 90, 78};
+  const int numberOfBatteries = sizeof(presentCapacities) / sizeof(presentCapacities[0]);
+  printf("Counting batteries by SoH (Additional Test)...\n");
+  struct CountsBySoH counts = countBatteriesByHealth(presentCapacities, numberOfBatteries);
+  assert(counts.healthy == 1); // One battery with SoH > 80%
+  assert(counts.exchange == 2); // Two batteries with SoH between 65% and 80%
+  assert(counts.failed == 2);   // Two batteries with SoH below 65%
+  printf("Additional Test Done counting :)\n");
+}
+
 
 int main() {
   testBucketingByHealth();
+  testAdditionalCase();
   return 0;
 }
